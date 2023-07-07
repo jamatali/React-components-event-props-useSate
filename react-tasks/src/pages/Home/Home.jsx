36ce9caef9c368b1947a43e0ae925e3e7016f020
unknown <SysAdmin@SCIPC-070.IPC.EDU>
2023-07-07 17:34:21 +0500

    feat:Dynamic functionality added

--- react-tasks/src/pages/Home/Home.jsx
+++ react-tasks/src/pages/Home/Home.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
 import Counter from "../../components/counter/Counter";
 
 export default function Home () {
-    return <Counter />
+    const [text, setText] = useState("");
+    const [input, setInput] = useState([]);
+   const handleChange = (event) => {
+    setText(event.target.value);
+   }
+   const handleClick = (dataChange) => {
+        dataChange = [...input];
+        dataChange.push(text);
+        setInput(dataChange);
+        
+   }
+
+
+    return (
+    <>
+    <input type="text" id="intext" placeholder="Enter text" onChange={handleChange}/>
+    <button type="submit" onClick={handleClick}>submit</button>
+     {input.map((h)=>{
+        return(
+            <Counter x = {h} />
+        )
+     })}
+    </>
+   
+    )
 }
